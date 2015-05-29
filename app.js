55e9ceaffb09f3737af91e23879bed1563dff6f8
Ben Meline <benmeline@gmail.com>
2015-05-29 11:42:30 -0500

    Allow CORS

--- app.js
+++ app.js
@@ -33,6 +33,24 @@ app.use(methodOverride(function(req, res){
   }
 }));
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use('/', routes);
 app.use('/products', products);
 app.use('/coupons', coupons);
